06af75b92f77bd0fb69865ef3f8e0265deb9d188
Alex Smola <alexsmola@alexs-air.home>
2022-04-20 09:46:58 +0100

    Added Option example

--- src/main/scala/com/smola/advanced/Advanced.scala
+++ src/main/scala/com/smola/advanced/Advanced.scala
@@ -12,5 +12,17 @@ object Advanced extends App {
   println(anotherValue)
   // Useful in infinite collections
 
+  // Pseudo-collections: Option, Try
+  def aMethodThatCanReturnNull(): String = "Hallo Norge!"
+  val anOption = Option(aMethodThatCanReturnNull()) // Some("Hallo Norge")
+  // Option = "collection" which contains at most one element: Some(value) or None
+
+  val stringProcessing = anOption match {
+    case Some(string) => s"I have obtained a valid string: $string"
+    case None => "I have obtained nothing"
+  }
+  // Can use map, flatMap, filter
+  println(stringProcessing)
+
 
 }
