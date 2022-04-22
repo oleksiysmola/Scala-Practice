96035ce9edfd833f8ede05ea9952362e914f76ee
Alex Smola <alexsmola@Alexs-MacBook-Air.local>
2022-04-22 09:06:33 +0100

    added SumAllIntegers test for case where argument is 0

--- src/main/scala/com/smola/basics/exercises/SumAllIntegers.scala
+++ src/main/scala/com/smola/basics/exercises/SumAllIntegers.scala
@@ -3,6 +3,7 @@ package com.smola.basics.exercises
 object SumAllIntegers extends App {
   def sumOfIntegers(n: Int): Int =
     if (n == 1) 1
-    else if (n < 1) throw new IllegalArgumentException()
+    else if (n == 0) 0  
+    else if (n < 0) throw new IllegalArgumentException()
     else n + sumOfIntegers(n - 1)
 }
--- src/test/scala/com/smola/basics/exercises/SumAllIntegersTest.scala
+++ src/test/scala/com/smola/basics/exercises/SumAllIntegersTest.scala
@@ -3,13 +3,22 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class SumAllIntegersTest extends AnyFunSuite {
   val underTest = SumAllIntegers
-  test("Can the function compute the sum of all the integers from 1 to 5") {
+  test("Can the function compute the sum of all the integers from 0 to 5") {
     // Given
     val summationLimit = 5
     // When
     val actual = underTest.sumOfIntegers(summationLimit)
     // Then
     val expected = 15
+    assert(actual == expected)
+  }
+  test("Returns 0 when given 0 as an argument") {
+    // Given
+    val summationLimit = 0
+    // When
+    val actual = underTest.sumOfIntegers(summationLimit)
+    // Then
+    val expected = 0
     assert(actual == expected)
   }
   test("Throws exception when argument is less than 1") {
